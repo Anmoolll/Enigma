87117bff04393314e18e07871d52f962299a91e7
Anmoolll <anmol.agarwal2022@vitstudent.ac.in>
2024-06-22 20:10:06 +0530

    Verification Email

---
+++ enigma/src/helpers/SendVerificationEmail.ts
@@ -0,0 +1,22 @@
+import { resend} from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmails";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse>{
+    try {
+        await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: email,
+            subject: 'Enigma | Verification Code',
+            react: VerificationEmail({username: username, otp: verifyCode}),
+          });
+        return{ success: true, message: 'Verification Email sent successfully' }
+    } catch (EmailError) {
+        console.log("Error sending verification email", EmailError);
+        return{ success: false, message: 'failed to send Verification Email' }
+    }
+}
