cd2d5db6dc914e690b72fba4d0f3785f4b3fde05
Anmoolll <anmol.agarwal2022@vitstudent.ac.in>
2025-06-04 20:19:14 +0530

    API/Route created for send messages

---
+++ enigma/src/app/api/send-message/route.ts
@@ -0,0 +1,44 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { Message } from "@/model/User";
+
+export async function POST(request:Request) {
+    await dbConnect()
+
+    const {username, content} = await request.json()
+    try {
+        const user = await UserModel.findOne({username})
+        if(!user){
+            return Response.json(
+            { message: 'User not found', success: false },
+            { status: 404 }
+        );
+        }
+
+        //is user accepting messages? 
+        if(!user.isAcceptingMessages){
+            return Response.json(
+            { message: 'User is not accepting messages', success: false },
+            { status: 403 }
+        );
+        }
+
+        const newMessage = {content, createdAt : new Date()}
+        user.message.push(newMessage as Message)
+        await user.save()
+
+        return Response.json(
+            { message: 'Message Sent Successfully', success: true },
+            { status: 404 }
+        );
+    } catch (error) {
+        console.log("Error sending messages", error)
+        return Response.json(
+            {
+                success : false,
+                message : "Internal Server Error occured"
+            },
+            {status : 500}
+        )
+    }
+}
