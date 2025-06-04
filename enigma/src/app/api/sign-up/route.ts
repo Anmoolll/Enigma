5d5431f014747162a9a9536d89aa15aae5650a80
Anmoolll <anmol.agarwal2022@vitstudent.ac.in>
2025-06-04 11:26:34 +0530

    debugged function call

--- enigma/src/app/api/sign-up/route.ts
+++ enigma/src/app/api/sign-up/route.ts
@@ -4,7 +4,7 @@ import bcrypt from "bcryptjs"
 import { sendVerificationEmail } from "@/helpers/SendVerificationEmail";
 
 export async function POST(request: Request){
-    await dbConnect;
+    await dbConnect();
 
     try {
         const {username, email, password} = await request.json()
